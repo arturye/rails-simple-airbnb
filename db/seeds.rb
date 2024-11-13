42f1429fbaa3e61508c4cc35bd618dc8e817747c
Artur Inoue <arturcorreainoue@gmail.com>
2024-11-13 17:15:29 +0000

    airbnb done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+ require 'faker'
+
+ 4.times do
+  Flat.create!(
+    name: Faker::Movies::HarryPotter.location,
+    address: Faker::Address.street_address,
+    description: 'Lorem ipsum odor amet, consectetuer adipiscing elt',
+    price_per_night: rand(0..200),
+    number_of_guests: rand(0..7)
+  )
+ end
